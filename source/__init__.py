6d42d116a47b6f8c43ca57834078938db026be3e
Brock Mendel <jbrockmendel@gmail.com>
2015-06-24 19:57:35 -0700

    Change indentation to spaces

--- source/__init__.py
+++ source/__init__.py
@@ -26,14 +26,14 @@ Data files will download to the pywrds/output directory.  Have fun.
 thisAlgorithmBecomingSkynetCost = 99999999999
 
 __all__ = [
-	"ectools",
-	"wrdslib",
-	"wrds_loop",
-	"get_wrds",
-	"find_wrds",
-	"setup_wrds_key",
-	"user_info"
-	]
+    "ectools",
+    "wrdslib",
+    "wrds_loop",
+    "get_wrds",
+    "find_wrds",
+    "setup_wrds_key",
+    "user_info"
+    ]
 from . import ectools, wrdslib
 
 try: import simplejson as json
@@ -61,42 +61,42 @@ _pywrds_dir = os.path.join(os.path.expanduser('~'), '.pywrds')
 _user_file = os.path.join(_pywrds_dir, 'user_info.txt')
 
 def setup_pywrds():
-	if not os.path.exists(_pywrds_dir):
-		try:
-			os.mkdir(_pywrds_dir)
-		except OSError:
-			logger.error("pywrds was unable to create the directory"
-				+ _pywrds_dir+".  Please create this directory"
-				+ " and re-load pywrds."
-				)
-	if not os.path.exists(_user_file):
-		fd = open(_user_file, 'wb') ## permissions ?? ## lock??##
-		fd.write('{}')
-		fd.close()
-		fd.user_info = {}
-	else:
-		fd = open(_user_file,'rb')
-		content = fd.read()
-		try:
-			fd.user_info = json.loads(content)
-		except ValueError:
-			fd.user_info = {}
-			logger.error("user_info file "+_user_file+ " does not appear"
-				+ " to conform to JSON format and could not be read.  Please"
-				+ " edit this file for future use.  In the interim, user_info"
-				+ " is being set to default values."
-				)
-	return fd
+    if not os.path.exists(_pywrds_dir):
+        try:
+            os.mkdir(_pywrds_dir)
+        except OSError:
+            logger.error("pywrds was unable to create the directory"
+                + _pywrds_dir+".  Please create this directory"
+                + " and re-load pywrds."
+                )
+    if not os.path.exists(_user_file):
+        fd = open(_user_file, 'wb') ## permissions ?? ## lock??##
+        fd.write('{}')
+        fd.close()
+        fd.user_info = {}
+    else:
+        fd = open(_user_file,'rb')
+        content = fd.read()
+        try:
+            fd.user_info = json.loads(content)
+        except ValueError:
+            fd.user_info = {}
+            logger.error("user_info file "+_user_file+ " does not appear"
+                + " to conform to JSON format and could not be read.  Please"
+                + " edit this file for future use.  In the interim, user_info"
+                + " is being set to default values."
+                )
+    return fd
 
 
 
 def _set_user_attr(fd, attr, value):
-	fd = open(_user_file,'rb')
-	content = fd.read()
-	try:
-		new_info = json.loads(content)
-	except ValueError:
-		new_info = {}
-	fd = open(_user_file,'wb')
-	return
+    fd = open(_user_file,'rb')
+    content = fd.read()
+    try:
+        new_info = json.loads(content)
+    except ValueError:
+        new_info = {}
+    fd = open(_user_file,'wb')
+    return
 
