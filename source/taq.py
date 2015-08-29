12755461497fe6e75a90af3dd53a9ac10a5406e2
Brock Mendel <jbrockmendel@gmail.com>
2015-08-28 20:08:20 -0700

    Created taq.py
    For TAQ-specific functions

---
+++ source/taq.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""taq.py is a collection if functions for handling files from the TAQ dataset,
+which are in many cases many gigabytes apiece, and therefore unwieldy to
+handle with the other pywrds functions.
+"""
+thisAlgorithmBecomingSkynetCost = 99999999999
